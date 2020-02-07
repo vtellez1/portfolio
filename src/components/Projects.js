64b0440c1dde32d1478fccf10748cd8324a60345
Vanessa <vanessatellez45@gmail.com>
2020-02-06 19:02:41 -0500

    added 2 projects in placeholders

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import todolistapp from '../ProfilePic/todolistapp.jpg';
+import topnineapp from '../ProfilePic/topnineapp.jpg';
+
 import {
     Card, Button, CardImg, CardTitle, CardText, CardDeck,
     CardSubtitle, CardBody
@@ -21,28 +24,26 @@ function Projects() {
       <h1>My Projects:</h1>
       <CardDeck>
       <Card>
-        <CardImg top width="100%" src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
+        <CardImg top width="100%" src={todolistapp} alt="Card image cap" />
         <CardBody>
-          <CardTitle>Project title</CardTitle>
-          <CardSubtitle>Built using: Languages/Framworks used</CardSubtitle>
-          <CardText>Description of project and my role. 2-3 bullets highlighting responsibilities, how technology was used,
-        or accomplishments..</CardText>
+          <CardTitle>To-Do List</CardTitle>
+          <CardSubtitle>Built using: React </CardSubtitle>
+          <CardText>Used class components to manage state for a to-do app. Able to add new to-do item, cross off completed, and clear all completed items.</CardText>
         <ButtonDiv>
-          <Button>View Project</Button>
-          <Button>GitHub Repo <i class="fab fa-github"></i></Button>
+          <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://vanessas-todo-list.netlify.com/'}}>View Project</Button>
+          <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://github.com/vtellez1/React-Todo'}}>GitHub Repo <i class="fab fa-github"></i></Button>
         </ButtonDiv>
         </CardBody>
       </Card>
       <Card>
-        <CardImg top width="100%" src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
+        <CardImg top width="100%" src={topnineapp} alt="Card image cap" />
         <CardBody>
-          <CardTitle>Project title</CardTitle>
-          <CardSubtitle>Built using:Languages/Framworks used</CardSubtitle>
-          <CardText>Description of project and my role. 2-3 bullets highlighting responsibilities, how technology was used,
-        or accomplishments..</CardText>
+          <CardTitle>My Top Nine</CardTitle>
+          <CardSubtitle>Built using: React </CardSubtitle>
+          <CardText>For this project, I was the React I developer. Top Nine allows you to choose your top 9 favorite things, edit/delete them, and view your friend's Top Nine.</CardText>
         <ButtonDiv>
-          <Button>View Project</Button>
-          <Button>GitHub Repo <i class="fab fa-github"></i></Button>
+          <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://mytopninefavs.netlify.com/'}}>View Project</Button>
+          <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://github.com/top-nine-pro'}}>GitHub Repo <i class="fab fa-github"></i></Button>
         </ButtonDiv>
         </CardBody>
       </Card>

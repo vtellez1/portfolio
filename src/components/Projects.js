20eb659aef7098cc363a38e3d42f3a65194aff24
Vanessa <vanessatellez45@gmail.com>
2020-06-04 15:55:19 -0400

    styling project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,6 +23,7 @@ import topnineapp from '../ProfilePic/topnineapp.jpg';
   padding: 1.5rem;
   margin: 1rem;
   border: black solid 1px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   `
   const CardImg = styled.img`
   width: 45%;

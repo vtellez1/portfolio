2428a2e888a0e1a79d0fafba39fba082239d0041
Vanessa <vanessatellez45@gmail.com>
2020-06-04 13:35:19 -0400

    added bullet descrption to project cards with overflow scroll

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,8 +38,12 @@ import topnineapp from '../ProfilePic/topnineapp.jpg';
 
   `
   const CardTitle = styled.h4``
-  const CardSubtitle = styled.h6``
-  const CardText = styled.p``
+  const CardText = styled.div`
+  height: 9.7rem;
+  overflow: scroll;
+  margin-bottom: 1rem;
+  padding-right: 1rem;
+  `
   const Button = styled.button``
 
   const ButtonDiv = styled.div`
@@ -57,9 +61,16 @@ function Projects() {
         <CardImg top width="100%" src={kanshaapp} alt="Kansha app screenshot" onClick={(e)=>{e.preventDefault(); window.location.href='https://kansharewards.com/'}}/>
         <CardBody>
           <CardTitle onClick={(e)=>{e.preventDefault(); window.location.href='https://kansharewards.com/'}}>Kansha Rewards</CardTitle>
-          <CardSubtitle>Built using: JavaScript | React | Redux | SASS | NodeJS | Postgres </CardSubtitle>
-          <CardText>Role: Front End and Back End Developer. </CardText>
-          <CardText>A workplace recognition program to appreciate peers through rewards and personalized messages.</CardText>
+          <CardText>
+            <h6>Built using: JavaScript | React | Redux | SASS | NodeJS | Postgres </h6>
+            <p>Role: Front End and Back End Developer. </p>
+            <p>A workplace recognition program to appreciate peers through rewards and personalized messages.</p>
+            <ul>
+              <li>Enhanced build-on project on a remote cross-functional team of 8 developers and a UX Designer over 8 weeks.</li>
+              <li>Developed application with front end and back end skills to reach product vision goals.</li>
+              <li>Implemented bulk-user upload capabilities with CSV files and reports on user recognition count.</li>
+            </ul>
+          </CardText>
 
 
         <ButtonDiv>
@@ -73,10 +84,17 @@ function Projects() {
         <CardImg top width="100%" src={simmerapp} alt="Simmer app screenshot" onClick={(e)=>{e.preventDefault(); window.location.href='https://deploy-inky.now.sh/home'}} />
         <CardBody>
           <CardTitle onClick={(e)=>{e.preventDefault(); window.location.href='https://deploy-inky.now.sh/home'}}>Simmr</CardTitle>
-          <CardSubtitle>Buit using: JavaScript | React | Redux | Material UI | NodeJS | Express</CardSubtitle>
-          <CardText>Role: Back End Developer</CardText>
-          <CardText>A professional platform for chefs to display their best recipes for the world to view with photos and ingredient lists. </CardText>
- 
+          <CardText>
+            <h6>Buit using: JavaScript | React | Redux | Material UI | NodeJS | Express</h6>
+            <p>Role: Back End Developer</p>
+            <p>A professional platform for chefs to display their best recipes for the world to view with photos and ingredient lists. </p>
+            <ul>
+              <li>Developed project on a remote team of 2 front-end developers, 2 backend developer, and 1 UI developer in 4 days.</li>
+              <li>Built Web API in coordination with front-end team and backend developer partner to meet client requirements.</li>
+              <li>Delivered an API using the REST architectural pattern, provided data persistence, incorporated authentication, and included testing.</li>
+            </ul>
+        </CardText>
+
         <ButtonDiv> 
         <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://deploy-inky.now.sh/home'}}>View Project</Button>
           <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://github.com/chefPassport/backend'}}>GitHub Repo <i class="fab fa-github"></i></Button>
@@ -88,10 +106,17 @@ function Projects() {
         <CardImg top width="100%" src={chefporfolioapp} alt="Chef Portfolio app screenshot" onClick={(e)=>{e.preventDefault(); window.location.href='https://objective-kalam-f1d625.netlify.app/'}}/>
         <CardBody>
           <CardTitle onClick={(e)=>{e.preventDefault(); window.location.href='https://objective-kalam-f1d625.netlify.app/'}}>Chef Portfolio</CardTitle>
-          <CardSubtitle>Buit using: JavaScript | React | Redux | SASS | NodeJS | Express</CardSubtitle>
-          <CardText>Role: Front End Developer</CardText>
-          <CardText>A professional platform for chefs to display their best recipes for the world to view with photos and ingredient lists. </CardText>
-          <CardText> Built project on a remote team of 4 front-end developers, 1 backend developer, and 2 UI developers in 4 days.</CardText>
+          <CardText>
+            <h6>Buit using: JavaScript | React | Redux | SASS | NodeJS | Express</h6>
+            <p>Role: Front End Developer</p>
+            <p>A professional platform for chefs to display their best recipes for the world to view with photos and ingredient lists. </p>
+            <p> Built project on a remote team of 4 front-end developers, 1 backend developer, and 2 UI developers in 4 days.</p>
+            <ul>
+              <li>Built project on a remote team of 4 front-end developers, 1 backend developer, and 2 UI developers in 4 days.</li>
+              <li>Developed state management for application and handled HTTP requests for all CRUD operations.</li>
+              <li> Implemented client-side authentication in collaboration with a backend developer.</li>
+            </ul>
+        </CardText>
         <ButtonDiv> 
         <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://objective-kalam-f1d625.netlify.app/'}}>View Project</Button>
           <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://github.com/ft-bw-chef-portfolio/front-end'}}>GitHub Repo <i class="fab fa-github"></i></Button>
@@ -103,9 +128,12 @@ function Projects() {
         <CardImg top width="100%" src={topnineapp} alt="top nine app screenshot" onClick={(e)=>{e.preventDefault(); window.location.href='https://mytopninefavs.netlify.com/'}} />
         <CardBody>
           <CardTitle onClick={(e)=>{e.preventDefault(); window.location.href='https://mytopninefavs.netlify.com/'}}>My Top Nine</CardTitle>
-          <CardSubtitle>Built using: React </CardSubtitle>
-          <CardText>Role: React I developer. </CardText>
-          <CardText>Top Nine allows you to choose your top 9 favorite things, edit/delete them, and view your friend's Top Nine.</CardText>
+          <CardText>
+          <h6>Built using: React </h6>
+          <p>Role: React I developer. </p>
+          <p>Top Nine allows you to choose your top 9 favorite things, edit/delete them, and view your friend's Top Nine.</p>
+          </CardText>
+
         <ButtonDiv>
           <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://mytopninefavs.netlify.com/'}}>View Project</Button>
           <Button onClick={(e)=>{e.preventDefault(); window.location.href='https://github.com/top-nine-pro'}}>GitHub Repo <i class="fab fa-github"></i></Button>

3106cef93c1445c6d589609e376caa8ad0be3357
Vanessa <vanessatellez45@gmail.com>
2020-06-11 12:32:16 -0400

    updated about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -91,7 +91,7 @@ function AboutMe(){
         <p>Hi, my name is <Title>Vanessa Tellez.</Title> I am a full-stack web developer based in <p><span style={{color: "crimson"}}><i class="fas fa-map-pin"></i></span> Charlotte, NC</p> I am passionate about continuous learning and enjoy problem solving. I am excited to apply my skills to real world problems and applications.     </p>
 
         <p>
-        When I'm not in front of my computer, I enjoy hiking, reading, visiting new coffee shops, and spending time with my family.
+        When I'm not in front of my computer, I enjoy hiking the Carolina mountains, reading, visiting new coffee shops (cold brews and dirty chai's all the way), and spending time with my family.
         </p>
         </AboutText>
 

12f70801893f016f90d82afccb616b1973aaaae0
Vanessa <vanessatellez45@gmail.com>
2020-08-20 11:51:29 -0400

    removed resume route path from App since its all on one page now

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { Switch, Route} from 'react-router-dom';
 
 import FullPage from './components/FullPage';
-import Resume from './components/Resume';
 import NavBar from './components/NavBar';
 
 
@@ -15,7 +14,6 @@ function App() {
       <NavBar/>
     <Switch>
         <Route exact path='/' component={FullPage}/>
-        <Route path='/resume' component={Resume} />
     </Switch>
     </div>
   );

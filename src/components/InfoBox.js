3c674134741f69aac94541175efb5e0e34e75287
Vanessa <vanessatellez45@gmail.com>
2020-09-11 19:16:44 -0400

    added target blank to infobox

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -14,17 +14,17 @@ function InfoBox() {
 
             <SocialsDiv>
             <SocialIcon>
-                <a href='https://github.com/vtellez1'><i style={{color: "black"}} class="fab fa-github fa-2x"></i></a>
+                <a target="_blank" href='https://github.com/vtellez1'><i style={{color: "black"}} class="fab fa-github fa-2x"></i></a>
                 <p>GitHub</p>
             </SocialIcon>
 
             <SocialIcon>
-                <a href='https://www.linkedin.com/in/tellezvanessa/'><i style={{color: "#0077B5"}} class="fab fa-linkedin fa-2x"></i></a>
+                <a target="_blank" href='https://www.linkedin.com/in/tellezvanessa/'><i style={{color: "#0077B5"}} class="fab fa-linkedin fa-2x"></i></a>
                 <p>LinkedIn</p>
             </SocialIcon>
         
             <SocialIcon>
-                <a href='https://twitter.com/vanessa_tellezz'><i style={{color: "#1DA1F2"}} class="fab fa-twitter-square fa-2x"></i></a>
+                <a target="_blank" href='https://twitter.com/vanessa_tellezz'><i style={{color: "#1DA1F2"}} class="fab fa-twitter-square fa-2x"></i></a>
                 <p>Twitter</p>
             </SocialIcon>
         </SocialsDiv>

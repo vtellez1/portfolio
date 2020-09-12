b06a257d6046512ec5487d023a0a33cde88b53ed
Vanessa <vanessatellez45@gmail.com>
2020-09-12 09:59:37 -0400

    added rel noopener noreferrer to links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,19 +7,19 @@ function Contact() {
       <Title>CONTACT ME</Title>
       <SocialsDiv>      
           <SocialIcon>
-                <a target="_blank" href='mailto:vanessatellez45@gmail.com'><i style={{color: "#484848"}} class="fas fa-envelope"> Email</i></a>
+                <a target="_blank" rel="noopener noreferrer" href='mailto:vanessatellez45@gmail.com'><i style={{color: "#484848"}} class="fas fa-envelope"> Email</i></a>
             </SocialIcon>
 
             <SocialIcon>
-                <a target="_blank" href='https://github.com/vtellez1'><i style={{color: "black"}} class="fab fa-github"> GitHub</i></a>
+                <a target="_blank" rel="noopener noreferrer" href='https://github.com/vtellez1'><i style={{color: "black"}} class="fab fa-github"> GitHub</i></a>
             </SocialIcon>
 
             <SocialIcon>
-                <a target="_blank" href='https://www.linkedin.com/in/tellezvanessa/'><i style={{color: "#0077B5"}}  class="fab fa-linkedin"> LinkedIn</i></a>
+                <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/tellezvanessa/'><i style={{color: "#0077B5"}}  class="fab fa-linkedin"> LinkedIn</i></a>
             </SocialIcon>
         
             <SocialIcon>
-                <a target="_blank" href='https://twitter.com/vanessa_tellezz'><i style={{color: "#1DA1F2"}} class="fab fa-twitter-square"> Twitter</i></a>
+                <a target="_blank" rel="noopener noreferrer" href='https://twitter.com/vanessa_tellezz'><i style={{color: "#1DA1F2"}} class="fab fa-twitter-square"> Twitter</i></a>
             </SocialIcon> 
         </SocialsDiv>
         <CopyRight>  Tellez - 2020</CopyRight>
